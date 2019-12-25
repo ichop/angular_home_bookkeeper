9c54f2f11c45a81f06c0cf4eddbcac62542eac82
Ivan <vanyachop@gmail.com>
2019-12-25 23:32:04 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hbk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'home-bookkeeper';
+}
